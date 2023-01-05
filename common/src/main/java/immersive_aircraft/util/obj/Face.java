70be7373616ab142b56b6a67f841b43032ce2c8d
Luke100000 <jonas.peche@aon.at>
2023-01-05 14:09:39 +0100

    refactored

--- common/src/main/java/immersive_aircraft/util/obj/Face.java
+++ common/src/main/java/immersive_aircraft/util/obj/Face.java
@@ -1,4 +1,4 @@
-package immersive_aircraft.util.obj.oobjloader;
+package immersive_aircraft.util.obj;
 
 // This code was written by myself, Sean R. Owens, sean at guild dot net,
 // and is released to the public domain. Share and enjoy. Since some
