a9e6c029a6c967c37001779f0a8aa2b6f56499c9
Luke100000 <jonas.peche@aon.at>
2023-01-24 17:42:19 +0100

    switched to less intrusive multi keybindings #14

--- changelog.md
+++ changelog.md
@@ -1,6 +1,7 @@
 # 0.3.0
 
 * Fixed non-pilot dismounting #17
+* Switched to less intrusive multi keybindings injection to restore mod compatibility
 
 # 0.2.0
 
--- common/src/main/java/immersive_aircraft/mixin/KeyBindingAccessorMixin.java
+++ common/src/main/java/immersive_aircraft/mixin/KeyBindingAccessorMixin.java
@@ -1,15 +1,11 @@
 package immersive_aircraft.mixin;
 
 import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(KeyBinding.class)
 public interface KeyBindingAccessorMixin {
-    @Accessor
-    InputUtil.Key getBoundKey();
-
     @Accessor
     int getTimesPressed();
 
