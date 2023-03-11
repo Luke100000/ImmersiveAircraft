217a635a761361491a398a6bffef6eb5ecc94e12
Luke100000 <jonas.peche@aon.at>
2023-03-11 14:36:37 +0100

    updated networkhandler

--- common/src/main/java/immersive_aircraft/Main.java
+++ common/src/main/java/immersive_aircraft/Main.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public final class Main {
+    public static final String SHORT_MOD_ID = "ic_air";
     public static final String MOD_ID = "immersive_aircraft";
     public static final Logger LOGGER = LogManager.getLogger();
     public static NetworkManager networkManager;
--- forge/src/main/java/immersive_aircraft/forge/cobalt/network/NetworkHandlerImpl.java
+++ forge/src/main/java/immersive_aircraft/forge/cobalt/network/NetworkHandlerImpl.java
@@ -12,7 +12,7 @@ import net.minecraftforge.network.simple.SimpleChannel;
 public class NetworkHandlerImpl extends NetworkHandler.Impl {
     private final String PROTOCOL_VERSION = "1";
     private final SimpleChannel channel = NetworkRegistry.newSimpleChannel(
-            new Identifier(Main.MOD_ID, "main"),
+            new Identifier(Main.SHORT_MOD_ID, "main"),
             () -> PROTOCOL_VERSION,
             PROTOCOL_VERSION::equals,
             PROTOCOL_VERSION::equals
@@ -24,7 +24,7 @@ public class NetworkHandlerImpl extends NetworkHandler.Impl {
     public <T extends Message> void registerMessage(Class<T> msg) {
         channel.registerMessage(id++, msg,
                 Message::encode,
-                b -> (T) Message.decode(b),
+                b -> (T)Message.decode(b),
                 (m, ctx) -> {
                     ctx.get().enqueueWork(() -> {
                         ServerPlayerEntity sender = ctx.get().getSender();
