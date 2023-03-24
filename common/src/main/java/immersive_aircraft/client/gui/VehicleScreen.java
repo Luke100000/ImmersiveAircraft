5c6748e0f9f8b4b9d81c7e789942f22129a7bc70
Luke100000 <jonas.peche@aon.at>
2023-03-24 22:48:14 +0100

    added missing tooltip

--- common/src/main/java/immersive_aircraft/client/gui/VehicleScreen.java
+++ common/src/main/java/immersive_aircraft/client/gui/VehicleScreen.java
@@ -92,7 +92,7 @@ public class VehicleScreen extends HandledScreen<VehicleScreenHandler> {
         // Slot tooltip
         if (focusedSlot != null && !focusedSlot.hasStack() && focusedSlot.inventory == handler.getVehicle().getInventory()) {
             VehicleInventoryDescription.Slot slot = handler.getVehicle().getInventoryDescription().getSlots().get(focusedSlot.getIndex());
-            if (slot.type == VehicleInventoryDescription.SlotType.BOOSTER || slot.type == VehicleInventoryDescription.SlotType.BOILER || slot.type == VehicleInventoryDescription.SlotType.UPGRADE || slot.type == VehicleInventoryDescription.SlotType.BANNER || slot.type == VehicleInventoryDescription.SlotType.WEAPON) {
+            if (slot.type == VehicleInventoryDescription.SlotType.DYE || slot.type == VehicleInventoryDescription.SlotType.BOOSTER || slot.type == VehicleInventoryDescription.SlotType.BOILER || slot.type == VehicleInventoryDescription.SlotType.UPGRADE || slot.type == VehicleInventoryDescription.SlotType.BANNER || slot.type == VehicleInventoryDescription.SlotType.WEAPON) {
                 this.renderTooltip(matrices, List.of(new TranslatableText("immersive_aircraft.slot." + slot.type.name().toLowerCase(Locale.ROOT))), Optional.empty(), mouseX, mouseY);
             }
         } else {
