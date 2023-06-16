7ddfe622ace985166b8ba60630e72056229836e3
Luke100000 <jonas.peche@aon.at>
2023-06-16 21:41:40 +0200

    Aircraft no longer break on autostep #54

--- changelog.md
+++ changelog.md
@@ -1,6 +1,7 @@
 # 0.5.1
 
 * Fixed glitch on sails when pausing
+* Aircraft no longer break on autostep
 
 # 0.5.0
 
--- common/src/main/java/immersive_aircraft/entity/VehicleEntity.java
+++ common/src/main/java/immersive_aircraft/entity/VehicleEntity.java
@@ -103,7 +103,7 @@ public abstract class VehicleEntity extends Entity {
 
     @Override
     public void setPitch(float pitch) {
-        float loops = (float)(Math.floor((pitch + 180f) / 360f) * 360f);
+        float loops = (float) (Math.floor((pitch + 180f) / 360f) * 360f);
         pitch -= loops;
         prevPitch -= loops;
         super.setPitch(pitch);
@@ -182,7 +182,7 @@ public abstract class VehicleEntity extends Entity {
         setDamageWobbleTicks(10);
         setDamageWobbleStrength(getDamageWobbleStrength() + amount * 10.0f / getDurability());
         emitGameEvent(GameEvent.ENTITY_DAMAGE, source.getAttacker());
-        boolean bl = source.getAttacker() instanceof PlayerEntity && ((PlayerEntity)source.getAttacker()).getAbilities().creativeMode;
+        boolean bl = source.getAttacker() instanceof PlayerEntity && ((PlayerEntity) source.getAttacker()).getAbilities().creativeMode;
         if (bl || getDamageWobbleStrength() > 40.0f) {
             if (!Config.getInstance().onlyPlayerCanDestroyAircraft || hasPassengers() || source.getAttacker() instanceof PlayerEntity) {
                 if (world.getGameRules().getBoolean(GameRules.DO_ENTITY_DROPS)) {
@@ -204,7 +204,7 @@ public abstract class VehicleEntity extends Entity {
 
     @Override
     public void onBubbleColumnSurfaceCollision(boolean drag) {
-        world.addParticle(ParticleTypes.SPLASH, getX() + (double)random.nextFloat(), getY() + 0.7, getZ() + (double)random.nextFloat(), 0.0, 0.0, 0.0);
+        world.addParticle(ParticleTypes.SPLASH, getX() + (double) random.nextFloat(), getY() + 0.7, getZ() + (double) random.nextFloat(), 0.0, 0.0, 0.0);
         if (random.nextInt(20) == 0) {
             world.playSound(getX(), getY(), getZ(), getSplashSound(), getSoundCategory(), 1.0f, 0.8f + 0.4f * random.nextFloat(), false);
         }
@@ -365,12 +365,12 @@ public abstract class VehicleEntity extends Entity {
         if (interpolationSteps <= 0) {
             return;
         }
-        double interpolatedX = getX() + (x - getX()) / (double)interpolationSteps;
-        double interpolatedY = getY() + (y - getY()) / (double)interpolationSteps;
-        double interpolatedZ = getZ() + (z - getZ()) / (double)interpolationSteps;
-        double interpolatedYaw = MathHelper.wrapDegrees(clientYaw - (double)getYaw());
-        setYaw(getYaw() + (float)interpolatedYaw / (float)interpolationSteps);
-        setPitch(getPitch() + (float)(clientPitch - (double)getPitch()) / (float)interpolationSteps);
+        double interpolatedX = getX() + (x - getX()) / (double) interpolationSteps;
+        double interpolatedY = getY() + (y - getY()) / (double) interpolationSteps;
+        double interpolatedZ = getZ() + (z - getZ()) / (double) interpolationSteps;
+        double interpolatedYaw = MathHelper.wrapDegrees(clientYaw - (double) getYaw());
+        setYaw(getYaw() + (float) interpolatedYaw / (float) interpolationSteps);
+        setPitch(getPitch() + (float) (clientPitch - (double) getPitch()) / (float) interpolationSteps);
 
         setPosition(interpolatedX, interpolatedY, interpolatedZ);
         setRotation(getYaw(), getPitch());
@@ -408,7 +408,7 @@ public abstract class VehicleEntity extends Entity {
 
                 position = position.add(0, passenger.getHeightOffset(), 0);
 
-                Vector4f worldPosition = transformPosition(transform, (float)position.x, (float)position.y, (float)position.z);
+                Vector4f worldPosition = transformPosition(transform, (float) position.x, (float) position.y, (float) position.z);
 
                 passenger.setPosition(worldPosition.getX(), worldPosition.getY(), worldPosition.getZ());
 
@@ -418,20 +418,20 @@ public abstract class VehicleEntity extends Entity {
                 copyEntityData(passenger);
                 if (passenger instanceof AnimalEntity && size > 1) {
                     int angle = passenger.getId() % 2 == 0 ? 90 : 270;
-                    passenger.setBodyYaw(((AnimalEntity)passenger).bodyYaw + (float)angle);
-                    passenger.setHeadYaw(passenger.getHeadYaw() + (float)angle);
+                    passenger.setBodyYaw(((AnimalEntity) passenger).bodyYaw + (float) angle);
+                    passenger.setHeadYaw(passenger.getHeadYaw() + (float) angle);
                 }
             }
         }
     }
 
     private Vec3d getDismountOffset(double vehicleWidth, double passengerWidth) {
-        double d = (vehicleWidth + passengerWidth + (double)1.0E-5f) / 2.0;
+        double d = (vehicleWidth + passengerWidth + (double) 1.0E-5f) / 2.0;
         float yaw = getYaw() + 90.0f;
-        float f = -MathHelper.sin(yaw * ((float)Math.PI / 180));
-        float g = MathHelper.cos(yaw * ((float)Math.PI / 180));
+        float f = -MathHelper.sin(yaw * ((float) Math.PI / 180));
+        float g = MathHelper.cos(yaw * ((float) Math.PI / 180));
         float h = Math.max(Math.abs(f), Math.abs(g));
-        return new Vec3d((double)f * d / (double)h, 0.0, (double)g * d / (double)h);
+        return new Vec3d((double) f * d / (double) h, 0.0, (double) g * d / (double) h);
     }
 
     @Override
@@ -447,10 +447,10 @@ public abstract class VehicleEntity extends Entity {
                 ArrayList<Vec3d> list = Lists.newArrayList();
                 double f = world.getDismountHeight(blockPos);
                 if (Dismounting.canDismountInBlock(f)) {
-                    list.add(new Vec3d(d, (double)blockPos.getY() + f, e));
+                    list.add(new Vec3d(d, (double) blockPos.getY() + f, e));
                 }
                 if (Dismounting.canDismountInBlock(g = world.getDismountHeight(blockPos2))) {
-                    list.add(new Vec3d(d, (double)blockPos2.getY() + g, e));
+                    list.add(new Vec3d(d, (double) blockPos2.getY() + g, e));
                 }
                 for (EntityPose entityPose : passenger.getPoses()) {
                     for (Vec3d vec3d2 : list) {
@@ -515,12 +515,16 @@ public abstract class VehicleEntity extends Entity {
         // Collision damage
         if (world.isClient && Config.getInstance().collisionDamage) {
             if (verticalCollision || horizontalCollision) {
-                float collision = (float)(prediction.subtract(getPos()).length() - Math.abs(getGravity()));
-                if (collision > 0.01f) {
-                    float repeat = 1.0f - (getDamageWobbleTicks() + 1) / 10.0f;
-                    if (repeat > 0.0001f) {
-                        float damage = collision * 20 * repeat * repeat;
-                        NetworkHandler.sendToServer(new CollisionMessage(damage));
+                double maxPossibleError = movement.length();
+                double error = prediction.distanceTo(getPos());
+                if (error <= maxPossibleError) {
+                    float collision = (float) (error - (verticalCollision ? Math.abs(getGravity()) : 0.0));
+                    if (collision > 0.01f) {
+                        float repeat = 1.0f - (getDamageWobbleTicks() + 1) / 10.0f;
+                        if (repeat > 0.0001f) {
+                            float damage = collision * 20 * repeat * repeat;
+                            NetworkHandler.sendToServer(new CollisionMessage(damage));
+                        }
                     }
                 }
             }
@@ -604,7 +608,7 @@ public abstract class VehicleEntity extends Entity {
     }
 
     protected Matrix4f getVehicleTransform() {
-        Matrix4f transform = Matrix4f.translate((float)getX(), (float)getY(), (float)getZ());
+        Matrix4f transform = Matrix4f.translate((float) getX(), (float) getY(), (float) getZ());
         transform.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(-getYaw()));
         transform.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(getPitch()));
         transform.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(getRoll()));
