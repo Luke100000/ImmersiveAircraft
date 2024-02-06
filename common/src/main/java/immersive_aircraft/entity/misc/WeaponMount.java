b5bba6df8ee02a4d62a1a3a382b76e448ce0dcd6
Luke100000 <jonas.peche@aon.at>
2024-02-06 12:07:06 +0100

    fixed missing class

--- common/src/main/java/immersive_aircraft/entity/misc/WeaponMount.java
+++ common/src/main/java/immersive_aircraft/entity/misc/WeaponMount.java
@@ -2,7 +2,6 @@ package immersive_aircraft.entity.misc;
 
 import com.mojang.math.Matrix4f;
 import net.minecraft.network.FriendlyByteBuf;
-import org.lwjgl.system.MemoryUtil;
 
 import java.nio.FloatBuffer;
 
@@ -10,7 +9,7 @@ public record WeaponMount(Matrix4f transform, boolean blocking) {
     public static final WeaponMount EMPTY = new WeaponMount(Matrix4f.createScaleMatrix(1.0f, 1.0f, 1.0f), false);
 
     public void encode(FriendlyByteBuf buffer) {
-        FloatBuffer floatValues = MemoryUtil.memAllocFloat(16);
+        FloatBuffer floatValues = FloatBuffer.allocate(16);
         transform.store(floatValues);
         for (int i = 0; i < 16; i++) {
             buffer.writeFloat(floatValues.get(i));
@@ -19,7 +18,7 @@ public record WeaponMount(Matrix4f transform, boolean blocking) {
     }
 
     public static WeaponMount decode(FriendlyByteBuf buffer) {
-        FloatBuffer floatValues = MemoryUtil.memAllocFloat(16);
+        FloatBuffer floatValues = FloatBuffer.allocate(16);
         for (int i = 0; i < 16; i++) {
             floatValues.put(buffer.readFloat());
         }
