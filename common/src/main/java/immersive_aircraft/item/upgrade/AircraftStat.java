124a4065e1cf73fc0e7974726cf829ff77c2e6db
Luke100000 <jonas.peche@aon.at>
2024-02-27 08:20:19 +0100

    fixed #121

--- common/src/main/java/immersive_aircraft/item/upgrade/AircraftStat.java
+++ common/src/main/java/immersive_aircraft/item/upgrade/AircraftStat.java
@@ -14,22 +14,22 @@ public record AircraftStat(String name, boolean positive, float defaultValue) {
 
     public static final AircraftStat ACCELERATION = register("acceleration", true);
     public static final AircraftStat DURABILITY = register("durability", true, 1.0f);
-    public static final AircraftStat FUEL = register("fuel", true);
+    public static final AircraftStat FUEL = register("fuel", false);
 
-    public static final AircraftStat FRICTION = register("friction", true);
+    public static final AircraftStat FRICTION = register("friction", false);
     public static final AircraftStat GLIDE_FACTOR = register("glideFactor", true);
     public static final AircraftStat LIFT = register("lift", true);
 
     public static final AircraftStat ROLL_FACTOR = register("rollFactor", true);
     public static final AircraftStat GROUND_PITCH = register("groundPitch", true);
 
-    public static final AircraftStat WIND = register("wind", true);
-    public static final AircraftStat MASS = register("mass", true, 1.0f);
+    public static final AircraftStat WIND = register("wind", false);
+    public static final AircraftStat MASS = register("mass", false, 1.0f);
 
-    public static final AircraftStat GROUND_FRICTION = register("groundFriction", true, 0.95f);
-    public static final AircraftStat ROTATION_DECAY = register("rotationDecay", true, 0.97f);
-    public static final AircraftStat HORIZONTAL_DECAY = register("horizontalDecay", true, 0.97f);
-    public static final AircraftStat VERTICAL_DECAY = register("verticalDecay", true, 0.97f);
+    public static final AircraftStat GROUND_FRICTION = register("groundFriction", false, 0.95f);
+    public static final AircraftStat ROTATION_DECAY = register("rotationDecay", false, 0.97f);
+    public static final AircraftStat HORIZONTAL_DECAY = register("horizontalDecay", false, 0.97f);
+    public static final AircraftStat VERTICAL_DECAY = register("verticalDecay", false, 0.97f);
 
     public static AircraftStat register(String name, boolean positive) {
         return register(name, positive, 0.0f);
