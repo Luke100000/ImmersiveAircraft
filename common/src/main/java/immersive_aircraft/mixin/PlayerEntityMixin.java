2bbbc590e19b34e9fb6d02df4f74b5d4eefcd935
Luke100000 <jonas.peche@aon.at>
2024-05-07 14:47:20 +0200

    fixed forge moment

--- common/src/main/java/immersive_aircraft/mixin/PlayerEntityMixin.java
+++ common/src/main/java/immersive_aircraft/mixin/PlayerEntityMixin.java
@@ -10,11 +10,10 @@ import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Player.class)
+@Mixin(value = Player.class, priority = 1100)
 public abstract class PlayerEntityMixin extends Entity {
     public PlayerEntityMixin(EntityType<?> type, Level world) {
         super(type, world);
@@ -41,11 +40,20 @@ public abstract class PlayerEntityMixin extends Entity {
         }
     }
 
+    /*
     @Redirect(method = "getDestroySpeed", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;onGround()Z"))
     public boolean immersive_aircraft$patchedGetDestroySpeed(Player player) {
         if (player.getRootVehicle() instanceof VehicleEntity) {
             return true;
         }
         return player.onGround();
+    }
+    */
+
+    @Inject(method = "getDestroySpeed", at = @At("RETURN"), cancellable = true)
+    public void immersive_aircraft$getDestroySpeed(CallbackInfoReturnable<Float> cir) {
+        if (this.getRootVehicle() instanceof VehicleEntity) {
+            cir.setReturnValue(cir.getReturnValueF() * 5.0f);
+        }
     }
 }
