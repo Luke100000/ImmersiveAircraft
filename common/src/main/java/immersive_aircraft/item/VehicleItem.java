0ad497d3420d7682e829a8e4c20e6509b5456fa0
Luke100000 <jonas.peche@aon.at>
2024-07-27 20:52:06 +0200

    placing a vehicle invalidly now prints an error message

--- common/src/main/java/immersive_aircraft/item/VehicleItem.java
+++ common/src/main/java/immersive_aircraft/item/VehicleItem.java
@@ -1,6 +1,7 @@
 package immersive_aircraft.item;
 
 import immersive_aircraft.entity.VehicleEntity;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
@@ -8,26 +9,19 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntitySelector;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
-import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
-import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
-import java.util.function.Predicate;
 
 public class VehicleItem extends DescriptionItem {
-    private static final Predicate<Entity> RIDERS = EntitySelector.NO_SPECTATORS.and(Entity::isPickable);
-
     public interface VehicleConstructor {
         VehicleEntity create(Level world);
     }
@@ -45,19 +39,8 @@ public class VehicleItem extends DescriptionItem {
         ItemStack itemStack = user.getItemInHand(hand);
         BlockHitResult hitResult = getPlayerPOVHitResult(world, user, ClipContext.Fluid.ANY);
         if (((HitResult) hitResult).getType() == HitResult.Type.MISS) {
+            error(user, "immersive_aircraft.tooltip.no_target");
             return InteractionResultHolder.pass(itemStack);
-        }
-
-        // Anti collision or something
-        Vec3 vec3d = user.getViewVector(1.0f);
-        List<Entity> list = world.getEntities(user, user.getBoundingBox().expandTowards(vec3d.scale(5.0)).inflate(1.0), RIDERS);
-        if (!list.isEmpty()) {
-            Vec3 vec3d2 = user.getEyePosition();
-            for (Entity entity : list) {
-                AABB box = entity.getBoundingBox().inflate(entity.getPickRadius());
-                if (!box.contains(vec3d2)) continue;
-                return InteractionResultHolder.pass(itemStack);
-            }
         }
 
         // Place the vehicle
@@ -70,6 +53,7 @@ public class VehicleItem extends DescriptionItem {
             entity.setYRot(user.getYRot());
 
             if (!world.noCollision(entity, entity.getBoundingBox())) {
+                error(user, "immersive_aircraft.tooltip.no_space");
                 return InteractionResultHolder.fail(itemStack);
             }
 
@@ -87,6 +71,10 @@ public class VehicleItem extends DescriptionItem {
         }
 
         return InteractionResultHolder.pass(itemStack);
+    }
+
+    private static void error(Player user, String message) {
+        user.displayClientMessage(Component.translatable(message).withStyle(ChatFormatting.RED), true);
     }
 
     @Override
