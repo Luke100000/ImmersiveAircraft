18009be1d781dae55cd10182e9e5f6404f505d94
JordanSekky <6587879+JordanSekky@users.noreply.github.com>
2025-12-17 17:56:10 -0500

    Body angle fixed

---
+++ common/src/main/java/immersive_aircraft/mixin/client/LivingEntityRenderStateMixin.java
@@ -0,0 +1,35 @@
+package immersive_aircraft.mixin.client;
+
+import immersive_aircraft.PassengerLivingEntityRenderState;
+import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(LivingEntityRenderState.class)
+public class LivingEntityRenderStateMixin implements PassengerLivingEntityRenderState {
+    @Unique
+    public Float immersive_aircraft$vehicleXRot = null;
+    @Unique
+    public Float immersive_aircraft$vehicleZRot = null;
+
+    @Override
+    public Float immersive_aircraft$getVehicleXRot() {
+        return immersive_aircraft$vehicleXRot;
+    }
+
+    @Override
+    public Float immersive_aircraft$getVehicleZRot() {
+        return immersive_aircraft$vehicleZRot;
+    }
+
+    @Override
+    public void immersive_aircraft$setVehicleXRot(Float f) {
+        this.immersive_aircraft$vehicleXRot = f;
+    }
+
+    @Override
+    public void immersive_aircraft$setVehicleZRot(Float f) {
+        this.immersive_aircraft$vehicleZRot = f;
+    }
+}
+
--- common/src/main/java/immersive_aircraft/mixin/client/LivingEntityRendererMixin.java
+++ common/src/main/java/immersive_aircraft/mixin/client/LivingEntityRendererMixin.java
@@ -2,10 +2,10 @@ package immersive_aircraft.mixin.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
+import immersive_aircraft.PassengerLivingEntityRenderState;
 import immersive_aircraft.entity.VehicleEntity;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,9 +17,22 @@ public class LivingEntityRendererMixin<T extends LivingEntity, S extends LivingE
 
     @Inject(method = "extractRenderState(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;F)V", at = @At("TAIL"))
     public void mixin$extractRenderState(T entity, S entityRenderState, float f, CallbackInfo ci) {
+        PassengerLivingEntityRenderState rs = (PassengerLivingEntityRenderState) entityRenderState;
         if (entity.getRootVehicle() != entity && entity.getRootVehicle() instanceof VehicleEntity vehicle) {
-            entityRenderState.bodyRot = Mth.wrapDegrees(entityRenderState.bodyRot - vehicle.getRoll(f));
-            entityRenderState.xRot = Mth.wrapDegrees(entityRenderState.xRot - vehicle.getViewXRot(f));
+            rs.immersive_aircraft$setVehicleXRot(vehicle.getViewXRot(f));
+            rs.immersive_aircraft$setVehicleZRot(vehicle.getRoll(f));
+        } else {
+            rs.immersive_aircraft$setVehicleXRot(null);
+            rs.immersive_aircraft$setVehicleZRot(null);
+        }
+    }
+
+    @Inject(method = "setupRotations", at = @At("TAIL"))
+    public void render(S livingEntityRenderState, PoseStack poseStack, float f, float g, CallbackInfo ci) {
+        PassengerLivingEntityRenderState rs = (PassengerLivingEntityRenderState) livingEntityRenderState;
+        if (rs.immersive_aircraft$getVehicleXRot() != null && rs.immersive_aircraft$getVehicleZRot() != null) {
+            poseStack.mulPose(Axis.XP.rotationDegrees(-rs.immersive_aircraft$getVehicleXRot()));
+            poseStack.mulPose(Axis.ZP.rotationDegrees(-rs.immersive_aircraft$getVehicleZRot()));
         }
     }
 }
